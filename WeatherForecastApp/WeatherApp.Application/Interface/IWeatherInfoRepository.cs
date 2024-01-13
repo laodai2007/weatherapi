d37e9e6bac8f48a4e035e827b23ca3f187670927
phuc_co <phuc@appsure.com.au>
2024-01-14 05:37:45 +0700

    Init proj

---
+++ WeatherForecastApp/WeatherApp.Application/Interface/IWeatherInfoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Application.Interface
+{
+    public class IWeatherInfoRepository
+    {
+    }
+}
